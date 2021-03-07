75b320db80546eecba6c2cb7eb5f5d2f15e0bb4c
Daniel Power <me@danielpower.ca>
2021-03-07 01:20:42 -0330

    Fixed missed name change

--- src/controllers/fireballPrediction.controller.js
+++ src/controllers/fireballPrediction.controller.js
@@ -1,10 +1,10 @@
 import dayjs from "dayjs";
 
-import FireballPredict from "../models/fireballPrediction.model.js";
+import FireballPrediction from "../models/fireballPrediction.model.js";
 
 export const findByDate = async (req, res) => {
   const year = parseInt(req.query.year, 10);
-  let predictions = await FireballPredict.findByDate(year);
+  let predictions = await FireballPrediction.findByDate(year);
   res.send(
     predictions.map(({ ip, des, dist, date, energy }) => ({
       ip,
