3ef8709bd7a2b1bd871f74d506ee9c90dd710fb0
Daniel Power <me@danielpower.ca>
2021-03-07 20:44:43 -0330

    Improve linting of mocha files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,15 +3,19 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
+    mocha: true,
   },
+  plugins: ["mocha"],
   extends: "eslint:recommended",
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: "module",
   },
   rules: {
-    indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
+    "mocha/no-exclusive-tests": "error",
+    "mocha/no-skipped-tests": "error",
+    indent: ["error", 2],
     quotes: ["error", "double"],
     semi: ["error", "always"],
   },
