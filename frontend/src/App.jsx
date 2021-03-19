453b5d434b629752c44e5a8c85b276aa437d1989
Daniel Power <me@danielpower.ca>
2021-03-19 00:27:13 -0230

    Remove frontend testing, add linting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(2);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(
-      () => setCount(Math.ceil((count + 20) / 1.5)),
-      1000,
-    );
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      Hello World
     </div>
   );
 }
