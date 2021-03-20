857898229735c0465abd1b87aa0a9a918cd2e019
Daniel Power <me@danielpower.ca>
2021-03-20 20:57:28 -0230

    dump

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Widget from './components/Widget';
 
-function App() {
+const App = () => {
+  const widgets = ['foo', 'bar', 'tar'];
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+      {widgets.map((content) => (
+        <Widget>{content}</Widget>
+      ))}
+    </>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Widget.jsx
+++ frontend/src/components/Widget.jsx
@@ -1,3 +1,6 @@
-const Widget = () => {};
+import React from 'react';
+import './Widget.css';
+
+const Widget = ({ children }) => <div className="widget">{children}</div>;
 
 export default Widget;
