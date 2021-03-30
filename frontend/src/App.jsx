554128b3bb1025a90dfcc3107b182d3fee6eb4c9
Taylorx20 <63863907+Taylorx20@users.noreply.github.com>
2021-03-30 20:40:34 -0230

    App changes for messenger and countdown

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Widget from './components/Widget';
 import Nhats from './components/widgets/Nhats';
+import Countdown from './components/widgets/Countdown';
+import Messenger from './components/widgets/messenger';
 
 const App = () => {
-  const widgets = [{ title: 'NHATS', key: 'nhats', component: Nhats }];
+  const widgets = [{ title: 'NHATS', key: 'nhats', component: Nhats }, 
+  { title: 'COUNTDOWN', key: 'countdown', component: Countdown }, 
+  { title: 'MESSENGER', key: 'messenger', component: Messenger }];
   return (
     <>
       {widgets.map((widget) => (
