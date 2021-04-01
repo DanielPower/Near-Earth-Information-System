5d6c83560fd20eb42da23b74d3b80aef8081f7e0
Taylorx20 <63863907+Taylorx20@users.noreply.github.com>
2021-04-01 21:19:54 -0230

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Widget from './components/Widget';
 import Nhats from './components/widgets/Nhats';
 import Countdown from './components/widgets/Countdown';
-import Messenger from './components/widgets/messenger';
+import Messenger from './components/widgets/Messenger';
 
 const App = () => {
-  const widgets = [{ title: 'NHATS', key: 'nhats', component: Nhats },
+  const widgets = [
+    { title: 'NHATS', key: 'nhats', component: Nhats },
     { title: 'COUNTDOWN', key: 'countdown', component: Countdown },
-    { title: 'MESSENGER', key: 'messenger', component: Messenger }];
+    { title: 'MESSENGER', key: 'messenger', component: Messenger },
+  ];
   return (
     <>
       {widgets.map((widget) => (
--- frontend/src/components/widgets/messenger.jsx
+++ frontend/src/components/widgets/messenger.jsx
@@ -2,62 +2,40 @@ import React from 'react';
 import useAxios from 'axios-hooks';
 import ScrollableList from '../ScrollableList/ScrollableList';
 
+const Messenger = () => {
+  const [{ data, loading, error }] = useAxios('http://localhost:3000/messages');
 
-
-//Today date function from https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
-let todayDate = new Date();
-const d = String(todayDate.getDate()).padStart(2, '0');
-const m = String(todayDate.getMonth() + 1).padStart(2, '0');
-const y = todayDate.getFullYear();
-todayDate = y + '/' + m + '/' + d;
-
-
-//useful for timestamping
-
-
-//date-min=1900-01-01&date-max=2100-01-01
-const messenger = () => {
-  const [{ data, loading, error }] = useAxios(
-    ('http://localhost:3000/messages'),
-  );
-  
+  console.log(error)
   if (loading) return 'loading';
   if (error) return 'error';
 
-  //const { data: } = data;
-  
   const { data: messages } = data;
-
-
-//need to test, and then ge the time between NEO and today
-//what to use instead of map(countdown)?
 
   return (
     <>
+      <label for="msgTitle">Title</label>
+      <input id="msgTitle"></input>
+      <label for="msgBody">Body</label>
+      <input id="msgBody"></input>
 
-    <label for = 'msgTitle'>Title</label>
-    <input id = 'msgTitle'></input>
-    <label for = 'msgBody'>Body</label>
-    <input id = 'msgBody'></input>
+      <label for="messageList"></label>
 
-    <label for = 'messageList'></label>
-
-{/* gonna get it up on the site and working, than i'll filter properly */}
-  <ScrollableList id = 'messageList'>
-      <div key={messages.messenger.title}>
-      <label>
-    <>{messages.messenger.title}</>
-    </label>
-    <body>
-    <>{messages.messenger.body}</>
-    </body>
-    <label>
-    <>{messages.messenger.date}</>
-    </label>
-    </div>
-  </ScrollableList>
+      {/* gonna get it up on the site and working, than i'll filter properly */}
+      <ScrollableList id="messageList">
+        <div key={messages.messenger.title + messages.messenger.date}>
+          <label>
+            <>{messages.messenger.title}</>
+          </label>
+          <body>
+            <>{messages.messenger.body}</>
+          </body>
+          <label>
+            <>{messages.messenger.date}</>
+          </label>
+        </div>
+      </ScrollableList>
     </>
   );
 };
 
-export default messenger;
+export default Messenger;
