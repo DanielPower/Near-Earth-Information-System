76c22f8d2d3e20a27289afd0dcdc467b0bcd9440
Daniel Power <me@danielpower.ca>
2021-04-02 01:52:54 -0230

    Nhats widget details page

--- frontend/snowpack.config.js
+++ frontend/snowpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
   plugins: ['@snowpack/plugin-react-refresh'],
   routes: [],
