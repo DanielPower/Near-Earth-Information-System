27290040c452394904603db3b31c6a4a5f759d51
Daniel Power <me@danielpower.ca>
2021-04-02 23:30:22 -0230

    Formatting fixes

--- frontend/src/components/widgets/Countdown.jsx
+++ frontend/src/components/widgets/Countdown.jsx
@@ -13,19 +13,14 @@ const Countdown = () => {
   const [distance, setDistance] = useState(0.5);
   const [tempDistance, setTempDistance] = useState(0.5);
 
-  let filterArr = [false, false, false];
-  
- 
-  
-    
   const [{ data, loading, error }] = useAxios({
     url: 'https://ssd-api.jpl.nasa.gov/cad.api',
     params: {
       'date-min': minDate,
       'date-max': maxDate,
-      'pha': filter === 'pha',
-      'nea': filter === 'nea',
-      'comet': filter === 'comet',
+      pha: filter === 'pha',
+      nea: filter === 'nea',
+      comet: filter === 'comet',
       'dist-max': distance,
     },
   });
@@ -40,18 +35,30 @@ const Countdown = () => {
       <label>NEO(Near Earth Objects</label>
       <div>
         <label>Object:</label>
-        <select name="object" value={filter} onChange={(event) => setFilter(event.target.value)} className={styles.textboxes}>
+        <select
+          name="object"
+          value={filter}
+          onChange={(event) => setFilter(event.target.value)}
+          className={styles.textboxes}
+        >
           <option>pha</option>
           <option>nea</option>
           <option>comet</option>
         </select>
-        
+
         <br />
         <label>Maximum distance:</label>
-        <input value={tempDistance} 
-          onChange={(event) => setTempDistance(event.target.value)} 
-          className={styles.textboxes}/>
-        <button type="button" value={distance} onClick={() => setDistance(tempDistance)} className={styles.textboxes}>
+        <input
+          value={tempDistance}
+          onChange={(event) => setTempDistance(event.target.value)}
+          className={styles.textboxes}
+        />
+        <button
+          type="button"
+          value={distance}
+          onClick={() => setDistance(tempDistance)}
+          className={styles.textboxes}
+        >
           Add
         </button>
         <br />
@@ -61,14 +68,17 @@ const Countdown = () => {
           type="date"
           value={minDate}
           onChange={(event) => setMinDate(event.target.value)}
-          className={styles.textboxes}/>
+          className={styles.textboxes}
+        />
         <input
           type="date"
           value={maxDate}
           onChange={(event) => setMaxDate(event.target.value)}
           className={styles.textboxes}
         />
-        <button type="button" className={styles.textboxes}>Add</button>
+        <button type="button" className={styles.textboxes}>
+          Add
+        </button>
         <br />
       </div>
 
@@ -91,7 +101,9 @@ const Countdown = () => {
             ],
             index,
           ) => (
-            <div key={index}>{`${des} will close-approach ${body} at: ${cd} at a distance of ${dist} AU`}</div>
+            <div key={index}>
+              {`${des} will close-approach ${body} at: ${cd} at a distance of ${dist} AU`}
+            </div>
           ),
         )}
       </ScrollableList>
